9295cc9dcbd37e9cb89c71bd88c41743b7eb4bbd
Aiden Smith <aidensmith150@gmail.com>
2024-12-16 15:25:58 +0000

    Remove files that should be ignored

--- src/Level-4-Individual-Project/Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/build/intermediates/cxx/RelWithDebInfo/6b333g5z/logs/arm64-v8a/build_command_targets.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-"C:\\Program Files\\Unity\\Hub\\Editor\\6000.0.26f1\\Editor\\Data\\PlaybackEngines\\AndroidPlayer\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\User\\Unity\\Level 4 Individual Project\\Level-4-Individual-Project\\src\\Level-4-Individual-Project\\.utmp\\RelWithDebInfo\\6b333g5z\\arm64-v8a" ^
-  game ^
-  swappywrapper
