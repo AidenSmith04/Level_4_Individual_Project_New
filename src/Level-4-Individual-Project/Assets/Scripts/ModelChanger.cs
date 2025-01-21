348a576303c3f41a02812dee1f44c6dd52f2fd77
Aiden Smith <aidensmith150@gmail.com>
2025-01-21 15:23:52 +0000

    Got the picture frames loaded in and added a rotation field to the SpatialAnchorSpawnerBuildingBlock code to allow for greater control in the showing of the models

--- src/Level-4-Individual-Project/Assets/Scripts/ModelChanger.cs
+++ src/Level-4-Individual-Project/Assets/Scripts/ModelChanger.cs
@@ -8,11 +8,13 @@ public class ModelChanger : MonoBehaviour
     public GameObject spatialAnchorController;
     public List<GameObject> prefabs = new List<GameObject>();
     public List<Vector3> displacements = new List<Vector3>();
+    public List<Quaternion> rotations = new List<Quaternion>();
 
     public void SwitchModel(int modelID)
     {
         SpatialAnchorSpawnerBuildingBlock spatialAnchorScript = spatialAnchorController.GetComponent<SpatialAnchorSpawnerBuildingBlock>();
         spatialAnchorScript.AnchorPrefab = prefabs[modelID];
         spatialAnchorScript.SetDisplacement(displacements[modelID]);
+        spatialAnchorScript.SetRotationOffset(rotations[modelID]);
     }
 }
